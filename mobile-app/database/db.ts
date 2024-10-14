68ec090afb9c70f40f551c317cdc79f418093da0
RASOLOFONIAINA Mickael <rasolofoniaina_mickael@outlook.fr>
2024-10-14 23:40:19 +0300

    Remove function to delete db

--- mobile-app/database/db.ts
+++ mobile-app/database/db.ts
@@ -2,9 +2,6 @@ import * as SQLite from "expo-sqlite";
 import { drizzle } from "drizzle-orm/expo-sqlite";
 import { INIT_DATABASE_SQL } from "./sql";
 
-// To remove
-SQLite.deleteDatabaseSync("app.db");
-
 export const expoDb = SQLite.openDatabaseSync("app.db");
 
 export const drizzleDb = drizzle(expoDb);
